492dc575f1b8f0390095659f82018584c713e0e9
Obed Murillo <obedmhg@gmail.com>
2018-12-20 12:21:39 -0700

    Adding Random Error

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,17 +2,20 @@ import React from 'react';
 import classes from  './Person.css';
 
 const person = (props) => {
+  const rnd = Math.random();
+  if(rnd > 0.98) {
+    throw new Error('Opps I did it again..');
+  }
+  return (
+    <div className={classes.Person}>
+    <h1 >I am {props.name}</h1>
+    <p>I am {props.age}</p>
+    <p>{props.children}</p>
+    <input type="text" onChange={props.changed} value={props.name}/>
+        <button className='remove-button' onClick={props.click}> Delete </button>
 
-return (
-  <div className={classes.Person}>
-  <h1 >I am {props.name}</h1>
-  <p>I am {props.age}</p>
-  <p>{props.children}</p>
-  <input type="text" onChange={props.changed} value={props.name}/>
-      <button className='remove-button' onClick={props.click}> Delete </button>
-
-    </div>
-  );
+      </div>
+    );
 }
 
 export default person;
